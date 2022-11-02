f1ff3b7e7e4e7fbe6726e1413e33a5193c5f7613
Vasu7777py <Subbannavarvasu@gmail.com>
2022-11-02 20:35:41 +0530

    small commit

--- apps/billingSys/src/app/app.module.ts
+++ apps/billingSys/src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { SettingsComponent } from './side-bar/settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     LoginPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SideBarComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
